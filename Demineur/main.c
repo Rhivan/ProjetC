3ac33db33bf1c8b029a2e9316b89443dfef3182d
abdelkader-islam-yahia <ayahia@gaming.tech>
2023-02-20 13:56:57 +0100

    sa place des bombes ici

--- Demineur/main.c
+++ Demineur/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-    printf  ("Hello World!\n");
+int main() {
+    int i, j;
+    char grid[5][5];
+    int num_bombs = 5;
+
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            grid[i][j] = 'X';
+        }
+    }
+
+    srand(time(NULL));
+    while (num_bombs > 0) {
+        int x = rand() % 5;
+        int y = rand() % 5;
+        if (grid[x][y] != 'B') {
+            grid[x][y] = 'B';
+            num_bombs--;
+        }
+    }
+
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("%c ", grid[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
