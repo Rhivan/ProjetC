342f00126519686ba22c0c415c15a6bc8fe04d26
Pepitovador <apreynat@gaming.tech>
2023-02-22 08:07:08 +0100

    gjdcjdg

--- Demineur/main.c
+++ Demineur/main.c
@@ -1,85 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-#define ROWS 10
-#define COLS 10
-#define MINE '*'
-#define EMPTY ' '
-#define COVERED '-'
+#define ROWS 10 // nombre de lignes
+#define COLS 10 // nombre de colonnes
 
-void initialize_board(char board[ROWS][COLS]) {
-    int i, j;
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            board[i][j] = COVERED;
-        }
-    }
-}
-
-void add_mines_to_board(char board[ROWS][COLS], int num_mines) {
-    int i, j, count = 0;
-    while (count < num_mines) {
-        i = rand() % ROWS;
-        j = rand() % COLS;
-        if (board[i][j] != MINE) {
-            board[i][j] = MINE;
-            count++;
-        }
-    }
-}
-
-void calculate_numbers(char board[ROWS][COLS]) {
-    int i, j;
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            if (board[i][j] != MINE) {
-                int count = 0;
-                if (i > 0 && j > 0 && board[i - 1][j - 1] == MINE) count++;
-                if (i > 0 && board[i - 1][j] == MINE) count++;
-                if (i > 0 && j < COLS - 1 && board[i - 1][j + 1] == MINE) count++;
-                if (j > 0 && board[i][j - 1] == MINE) count++;
-                if (j < COLS - 1 && board[i][j + 1] == MINE) count++;
-                if (i < ROWS - 1 && j > 0 && board[i + 1][j - 1] == MINE) count++;
-                if (i < ROWS - 1 && board[i + 1][j] == MINE) count++;
-                if (i < ROWS - 1 && j < COLS - 1 && board[i + 1][j + 1] == MINE) count++;
-                if (count > 0) {
-                    board[i][j] = '0' + count;
-                }
-                else {
-                    board[i][j] = EMPTY;
-                }
-            }
-        }
-    }
-}
-
-void print_board(char board[ROWS][COLS]) {
-    int i, j;
-    printf("  ");
-    for (i = 0; i < COLS; i++) {
-        printf(" %d ", i);
+void afficher_grille(char grille[ROWS][COLS]) {
+    printf("   ");
+    for (int i = 0; i < COLS; i++) {
+        printf(" %2d ", i+1);
     }
     printf("\n");
-    for (i = 0; i < ROWS; i++) {
-        printf("%d ", i);
-        for (j = 0; j < COLS; j++) {
-            printf("|%c|", board[i][j]);
+    printf("   ");
+    for (int i = 0; i < COLS; i++) {
+        printf("----");
+    }
+    printf("-\n");
+
+    for (int i = 0; i < ROWS; i++) {
+        printf("%2d |", i+1);
+        for (int j = 0; j < COLS; j++) {
+            printf(" %c |", grille[i][j]);
         }
         printf("\n");
+        printf("   ");
+        for (int j = 0; j < COLS; j++) {
+            printf("----");
+        }
+        printf("-\n");
     }
 }
 
 int main() {
-    char board[ROWS][COLS];
-    int num_mines = 10;
+    char grille[ROWS][COLS];
 
-    srand(time(NULL));
+    // initialiser la grille avec des cases vides
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            grille[i][j] = '-';
+        }
+    }
 
-    initialize_board(board);
-    add_mines_to_board(board, num_mines);
-    calculate_numbers(board);
-    print_board(board);
+    // afficher la grille
+    afficher_grille(grille);
 
     return 0;
 }
