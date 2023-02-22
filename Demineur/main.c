1ecb2a250df08c83be8bc37eb14286db8b0f1fe0
Pepitovador <apreynat@gaming.tech>
2023-02-22 09:59:14 +0100

    bombe ajout

--- Demineur/main.c
+++ Demineur/main.c
@@ -2,11 +2,15 @@
 
 #define ROWS 10 // nombre de lignes
 #define COLS 10 // nombre de colonnes
+#define BOMB 'B' 
+#define CACHE '+'
+#define EMPTY ' '
 
-void afficher_grille(char grille[ROWS][COLS]) {
+void afficher_grille(char grille[ROWS][COLS])
+{
     printf("   ");
     for (int i = 0; i < COLS; i++) {
-        printf(" %2d ", i+1);
+        printf(" %2d ", i + 1);
     }
     printf("\n");
     printf("   ");
@@ -16,9 +20,14 @@ void afficher_grille(char grille[ROWS][COLS]) {
     printf("-\n");
 
     for (int i = 0; i < ROWS; i++) {
-        printf("%2d |", i+1);
+        printf("%2d |", i + 1);
         for (int j = 0; j < COLS; j++) {
-            printf(" %c |", grille[i][j]);
+            if (grille[i][j] == BOMB) {
+                printf(" %c |", CACHE);
+            }
+            else {
+                printf(" %c |", grille[i][j]);
+            }
         }
         printf("\n");
         printf("   ");
@@ -29,17 +38,41 @@ void afficher_grille(char grille[ROWS][COLS]) {
     }
 }
 
-int main() {
-    char grille[ROWS][COLS];
 
-    // initialiser la grille avec des cases vides
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            grille[i][j] = '-';
+void plant_bomb(char grille[ROWS][COLS] , int num_mines)
+{
+    int i, j, count = 0;
+    while (count < num_mines) {
+        i = rand() % ROWS;
+        j = rand() % COLS;
+        if (grille[i][j] != BOMB ) {
+            grille[i][j] = BOMB  ;
+            count++;
         }
     }
+}
+
+void initialize_grille(char grille[ROWS][COLS])
+{
+    int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            grille[i][j] = CACHE;
+        }
+    }
+}
+
+
+int main() 
+{
+    char grille[ROWS][COLS];
+    int num_mines = 10;
+
 
     // afficher la grille
+   
+    initialize_grille(grille);
+    plant_bomb(grille, num_mines);
     afficher_grille(grille);
 
     return 0;
